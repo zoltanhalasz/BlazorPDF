ea71780b94c06a7a6c6c83a6b68cfd2734ec869f
Zoltan Halasz <42749923+zoltanhalasz@users.noreply.github.com>
2020-12-04 10:55:19 +0200

    Add project files.

---
+++ BlazorPDF/Data/RptStudent.cs
@@ -0,0 +1,126 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPDF.Data
+{
+    public class RptStudent
+    {
+        int _maxColumn = 3;
+        Document _document;
+        PdfPTable _pdfTable = new PdfPTable(3);
+        PdfPCell _pdfCell;
+        Font _fontStyle;
+        MemoryStream _memStream = new MemoryStream();
+        List<Student> _oStudents = new List<Student>();
+
+        
+        public byte[] Report (List<Student> myList)
+        {
+            _oStudents = myList;
+
+            _document = new Document(PageSize.A4, 10f, 10f, 20f, 30f);
+            _pdfTable.WidthPercentage = 100;
+            _pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+            _fontStyle = FontFactory.GetFont("Tahoma", 8f, 1);
+            PdfWriter.GetInstance(_document, _memStream);
+            _document.Open();
+            float[] sizes = new float[_maxColumn];
+            for (int i = 0; i< _maxColumn; i++)
+            {
+                if (i == 0) sizes[i] = 50;
+                else sizes[i] = 100;
+            }
+            _pdfTable.SetWidths(sizes);
+
+            this.ReportHeader();
+
+            //_document.Add(new Paragraph("\n"));
+            //_document.Add(new Paragraph("\n"));
+
+            this.ReportBody();
+            _pdfTable.HeaderRows = 2;
+            _document.Add(_pdfTable);
+           
+            _document.Close();
+
+            return _memStream.ToArray();
+        }
+
+        private void ReportHeader()
+        {
+            _fontStyle = FontFactory.GetFont("Tahoma", 18f, 1);
+            _pdfCell = new PdfPCell(new Phrase("Birthday", _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+
+            _pdfCell = new PdfPCell(new Phrase("Student Name", _fontStyle));
+            _pdfCell.Colspan = _maxColumn;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.Border = 0;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            
+            _pdfTable.CompleteRow();     
+      
+        }
+
+        private void ReportBody()
+        {
+            _fontStyle = FontFactory.GetFont("Tahoma", 18f, 1);
+             var fontStyle = FontFactory.GetFont("Tahoma", 9f, 0);
+
+            _pdfCell = new PdfPCell(new Phrase("Id", _fontStyle));            
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.Gray;            
+            _pdfTable.AddCell(_pdfCell);
+
+            _pdfCell = new PdfPCell(new Phrase("Name", _fontStyle));
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.Gray;
+            _pdfTable.AddCell(_pdfCell);
+
+            _pdfCell = new PdfPCell(new Phrase("BirthDay", _fontStyle));
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.Gray;
+            _pdfTable.AddCell(_pdfCell);
+
+            _pdfTable.CompleteRow();
+
+            int nSL = 1;
+            foreach (var stud in _oStudents)
+            {
+                _pdfCell = new PdfPCell(new Phrase((nSL++).ToString(), fontStyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.LightGray;
+                _pdfTable.AddCell(_pdfCell);
+
+                _pdfCell = new PdfPCell(new Phrase(stud.Name, fontStyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.White;
+                _pdfTable.AddCell(_pdfCell);
+
+                _pdfCell = new PdfPCell(new Phrase(stud.BirthDay.ToShortDateString(), fontStyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.White;
+                _pdfTable.AddCell(_pdfCell);
+
+                _pdfTable.CompleteRow();
+            }
+        }
+    }
+}
---
+++ BlazorPDF/Data/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPDF.Data
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string Name { get; set; }
+        public DateTime BirthDay { get; set; }
+    }
+}
